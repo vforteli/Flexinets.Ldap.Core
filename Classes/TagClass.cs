e531e4375e1f85578c11a7fb43f7932649cd95c3
verne <verner@fortelius.net>
2017-07-29 21:57:17 +0300

    Initial commit

---
+++ Classes/TagClass.cs
@@ -0,0 +1,10 @@
+﻿namespace Flexinets.Ldap.Core
+{
+    public enum TagClass
+    {
+        Universal = 0,
+        Application = 1,
+        Context = 2,
+        Private = 3
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Flexinets.Ldap.Core
